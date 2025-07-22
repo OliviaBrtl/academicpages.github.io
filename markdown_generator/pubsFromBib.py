9e673996e1013a9ff68d91cd8692339f5fdc49a0
Olivia Bertelli <160524154+OliviaBrtl@users.noreply.github.com>
2025-07-22 18:48:24 +0200

    Update pubsFromBib.py

--- markdown_generator/pubsFromBib.py
+++ markdown_generator/pubsFromBib.py
@@ -26,21 +26,15 @@ import re
 
 #todo: incorporate different collection types rather than a catch all publications, requires other changes to template
 publist = {
-    "proceeding": {
-        "file" : "proceedings.bib",
-        "venuekey": "booktitle",
-        "venue-pretext": "In the proceedings of ",
-        "collection" : {"name":"publications",
-                        "permalink":"/publication/"}
-        
-    },
-    "journal":{
-        "file": "pubs.bib",
-        "venuekey" : "journal",
-        "venue-pretext" : "",
-        "collection" : {"name":"publications",
-                        "permalink":"/publication/"}
-    } 
+    "article": {
+        "file": "../files/Publications.bib",
+        "venuekey": "journal",
+        "venue-pretext": "",
+        "collection": {
+            "name": "publications",
+            "permalink": "/publication/"
+        }
+    }
 }
 
 html_escape_table = {
